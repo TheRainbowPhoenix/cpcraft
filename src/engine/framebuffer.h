ae5f7553ce657850305291c3737b77d7961f4002
PC <TheRainbowPhoenix@users.noreply.github.com>
2026-06-26 20:04:44 +0200

    test add lcd_data_port

--- src/engine/framebuffer.h
+++ src/engine/framebuffer.h
@@ -70,6 +70,10 @@
 #include <sdk/os/lcd.h>
 #include "builtins.h"
 
+#ifndef __sh__
+volatile uint16_t *const lcd_data_port = (volatile uint16_t *)0xB4000000;
+#endif
+
 /* Do not include sdk/calc/calc.h here unless framebuffer really needs it.
  * That header has C++ overloads and should not be pulled into generic C headers.
  */
