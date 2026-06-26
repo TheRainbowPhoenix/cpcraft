33b6d3264b8f9f7f4c9d76b230cbe8740338be67
PC <TheRainbowPhoenix@users.noreply.github.com>
2026-06-26 22:04:32 +0200

    quick fix MSVC

--- src/engine/builtins.h
+++ src/engine/builtins.h
@@ -13,6 +13,24 @@
  */
 #pragma once
 
+#if defined(_MSC_VER) && !defined(__clang__)
+#ifndef __attribute__
+#define __attribute__(x)
+#endif
+
+#ifndef __builtin_expect
+#define __builtin_expect(x, expected) (x)
+#endif
+
+#ifndef __builtin_prefetch
+#define __builtin_prefetch(addr, rw, locality) ((void)0)
+#endif
+
+#ifndef __builtin_assume_aligned
+#define __builtin_assume_aligned(ptr, align) (ptr)
+#endif
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
